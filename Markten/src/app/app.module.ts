73b5cdeb2102d93402fe10848ce6384b150bcbc4
Tehpopss <73282540+Tehpopss@users.noreply.github.com>
2021-11-16 11:58:55 +0100

    Adding a toolbar and a card to Angular

--- Markten/src/app/app.module.ts
+++ Markten/src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Import toolbar and card modules
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatCardModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
